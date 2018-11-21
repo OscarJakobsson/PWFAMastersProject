a313aa7e1f598b60c58e1ba1bd5f03f505b3bd79
Oscar Lars Jakobsson <oscar.lars.jakobsson@cern.ch>
2018-11-21 23:56:22 +0000

    m

---
+++ Codes/downloadfiles.py
@@ -0,0 +1,7 @@
+for i in range(101,201):
+	if (i<10):
+		print('000%d.sdf,'%i, end ="")
+	if (i<100 and i>9):
+		print('00%d.sdf,'%i, end ="")
+	if (i<1000 and i>99):
+		print('0%d.sdf,'%i, end ="")		
